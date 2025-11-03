b94babdaca1c6b3667f6fafcf4eb37651cac546e
perez-eduardo <eduardo.perez.student@gmail.com>
2025-11-03 13:47:54 -0800

    Adjust random length range for credit card tests

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ from credit_card_validator import credit_card_validator
 class TestCreditCardValidator(unittest.TestCase):
     def test_all_lengths(self):
         for _ in range(700000):
-            length = random.randint(15, 16)
+            length = random.randint(13, 17)
             card = ''.join(str(random.randint(0, 9)) for _ in range(length))
             credit_card_validator(card)
 
