d07b2aec64612e7c4ef653c909a2ddcb6334bf30
perez-eduardo <eduardo.perez.student@gmail.com>
2025-11-03 02:31:23 -0800

    Increase test iterations for credit card validation

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
     def test_all_lengths(self):
-        for _ in range(350000):
+        for _ in range(900000):
             length = random.randint(1, 30)
             card = ''.join(str(random.randint(0, 9)) for _ in range(length))
             credit_card_validator(card)
