ce3c5f385921c90f9fa64ea9f712caebbfd97fe4
perez-eduardo <eduardo.perez.student@gmail.com>
2025-11-03 02:23:53 -0800

    Refactor credit card validation tests
    Removed specific digit tests and consolidated into a valid range test.

--- tests.py
+++ tests.py
@@ -4,29 +4,9 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCreditCardValidator(unittest.TestCase):
-
-
-
-
-    def test_fifteen_digits(self):
-        for _ in range(15000):
-            card = ''.join(str(random.randint(0, 9)) for _ in range(15))
-            credit_card_validator(card)
-
-    def test_sixteen_digits(self):
-        for _ in range(15000):
-            card = ''.join(str(random.randint(0, 9)) for _ in range(16))
-            credit_card_validator(card)
-
-    def test_long_valid(self):
-        for _ in range(15000):
-            length = random.randint(17, 19)
-            card = ''.join(str(random.randint(0, 9)) for _ in range(length))
-            credit_card_validator(card)
-
-    def test_above_maximum(self):
-        for _ in range(15000):
-            length = random.randint(20, 30)
+    def test_valid_range(self):
+        for _ in range(100000):
+            length = random.randint(10, 19)
             card = ''.join(str(random.randint(0, 9)) for _ in range(length))
             credit_card_validator(card)
 
