d9b57ee19cf0444ecc47e049f971e085f0bf4875
perez-eduardo <eduardo.perez.student@gmail.com>
2025-11-03 13:41:47 -0800

    Reduce test iterations and card length range

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
     def test_all_lengths(self):
-        for _ in range(800000):
-            length = random.randint(9, 20)
+        for _ in range(700000):
+            length = random.randint(15, 16)
             card = ''.join(str(random.randint(0, 9)) for _ in range(length))
             credit_card_validator(card)
 
