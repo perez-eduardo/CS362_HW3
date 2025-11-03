268ed82a3a6c2fbb0239f324bc94ab5f4500abcf
perez-eduardo <eduardo.perez.student@gmail.com>
2025-11-03 02:27:58 -0800

    Modify test to include all lengths from 1 to 30

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCreditCardValidator(unittest.TestCase):
-    def test_valid_range(self):
-        for _ in range(100000):
-            length = random.randint(10, 19)
+    def test_all_lengths(self):
+        for _ in range(150000):
+            length = random.randint(1, 30)
             card = ''.join(str(random.randint(0, 9)) for _ in range(length))
             credit_card_validator(card)
 
